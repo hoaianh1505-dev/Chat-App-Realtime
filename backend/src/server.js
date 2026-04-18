8821acf9b92248c20736ff1e541bca13a29a97b8
Do Hoai Anh <anhd78428@gmail.com>
2026-04-18 12:48:54 +0700

    finish friendRoute

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
+import friendRoute from "./routes/friendRoute.js";
+import messageRoute from "./routes/mesageRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
 import cors from "cors";
@@ -23,7 +25,8 @@ app.use("/api/auth", authRoute);
 // private routes
 app.use(protectedRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/friends", friendRoute);
+app.use("/api/messages", messageRoute);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`server bắt đầu trên cổng ${PORT}`);
