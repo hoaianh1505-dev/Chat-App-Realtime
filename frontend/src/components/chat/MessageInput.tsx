9e7c3e3acd4738c91ab72ce9c0b9eecb19884082
Do Hoai Anh <anhd78428@gmail.com>
2026-04-20 15:30:46 +0700

    finish UI Input send message

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useAuthStore } from "@/stores/useAuthStore"
+import type { Conversation } from "@/types/chat"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { ImagePlus, Send } from "lucide-react"
+import { Input } from "../ui/input"
 
-const MessageInput = () => {
+const MessageInput = ({ selectedConvo }: { selectedConvo: Conversation }) => {
+    const { user } = useAuthStore()
+    const [value, setValue] = useState("")
+    if (!user) return;
+
     return (
-        <div>
-            MessageInput
+        <div className="flex items-center gap-2 min-h-[56px] bg-background">
+            <Button
+                variant="ghost"
+                size="icon"
+                className="hover:bg-primary/10 transition-smooth"
+            >
+                <ImagePlus className="size-4 " />
+            </Button>
+            <div className="flex-1 relative ">
+                <Input value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder="Soạn tin nhắn"
+                    className="pr-20 h-9 nbg-white border-border/50 focus:border-primary/50 transition-smooth resize-none"
+                ></Input>
+                <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center gap-1 ">
+                    <Button
+                        asChild
+                        variant="ghost"
+                        size="icon"
+                        className="size-8 hover:bg-primary/10 transition-smooth">
+                        <div className="">
+                            {/* emoji */}
+                        </div>
+                    </Button>
+                    <Button className="bg-gradient-chat hover:shadow-glow transition-smooth hover:scale-105 " disabled={!value.trim()}
+                    >
+                        <Send className="size-4 text-white " />
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
